eac83e2ff622eaf1c773c3d5dceaa6170f1c23cb
btsd1 <108137547+btsd1@users.noreply.github.com>
2024-10-05 15:38:14 -0400

    sequelize transaction wrapper

--- sequelize.js
+++ sequelize.js
@@ -1,14 +1,23 @@
 import { Sequelize } from "sequelize";
 import config from './config.js'
 
-const sequelize = new Sequelize(`postgresql://postgres.fgideclrpjutgkejlsys:${config.pgPassword}@aws-0-us-east-1.pooler.supabase.com:${config.pgPort}/postgres`)
+export const sequelize = new Sequelize(`postgresql://postgres.fgideclrpjutgkejlsys:${config.pgPassword}@aws-0-us-east-1.pooler.supabase.com:${config.pgPort}/postgres`)
 
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+export const transaction = async (sqlizeCallback) => {
+    try {
+        const result = await sequelize.transaction(async t => {
+            return await sqlizeCallback(t);
+        });
+        return result
+        // If the execution reaches this line, the transaction has been committed successfully
+        // `result` is whatever was returned from the transaction callback (the `user`, in this case)
+      } catch (error) {
+        console.log(`The transaction has been rolled back because of the following error: ${error}`)
+        throw error;
+        // If the execution reaches this line, an error occurred.
+        // The transaction has already been rolled back automatically by Sequelize!
+      }
+}
 
   //sequelize.close() CLOSES CONNECTION
   
